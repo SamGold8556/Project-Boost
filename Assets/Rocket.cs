4151881874860967e3945a864e785e23069df172
Sam Gold <SamGold8556@gmail.com>
2018-08-29 21:41:15 -0600

    Rocket Modeled

---
+++ Assets/Rocket.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessUpdate();
+		
+	}
+
+    private void ProcessUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("Space Pressed");
+        }
+    }
+}
